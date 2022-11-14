72325d0cab649da3bb5ed266720a478a85e16f2b
dogyeong <shroad1802@naver.com>
2022-11-15 00:18:43 +0900

    chore: update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: 'static',
   typescript: {
     strict: true,
   },
