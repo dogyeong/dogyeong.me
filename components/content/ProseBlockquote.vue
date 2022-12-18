65b20816ab9fb759a095b14873f97a2981f1e053
dogyeong <shroad1802@naver.com>
2022-12-18 09:33:47 +0900

    feat: add blockquote style

--- components/content/ProseBlockquote.vue
+++ components/content/ProseBlockquote.vue
@@ -1,5 +1,21 @@
 <template>
-  <blockquote>
+  <blockquote :class="$style.blockquote">
     <slot />
   </blockquote>
 </template>
+
+<style lang="scss" module>
+.blockquote {
+  background-color: $grey-8;
+  margin-bottom: 2.5rem;
+  padding: 2rem;
+  border: 0;
+  border-radius: 0.5rem;
+  font-style: italic;
+  font-weight: 400;
+
+  p {
+    margin: 0;
+  }
+}
+</style>
