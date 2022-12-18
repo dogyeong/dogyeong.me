3467e90421a114c23891678f2d83f88b165fd734
dogyeong <shroad1802@naver.com>
2022-12-18 09:38:07 +0900

    feat: add strong style

--- components/content/ProseStrong.vue
+++ components/content/ProseStrong.vue
@@ -1,5 +1,12 @@
 <template>
-  <strong>
+  <strong :class="$style.strong">
     <slot />
   </strong>
 </template>
+
+<style lang="scss" module>
+.strong {
+  font-weight: 500;
+  color: $grey-3;
+}
+</style>
