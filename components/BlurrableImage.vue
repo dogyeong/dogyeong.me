3ed952304627bca83c6eb783cf26094ff725648d
dogyeong <shroad1802@naver.com>
2023-03-12 16:24:34 +0900

    feat: optimize image loading experience

--- components/BlurrableImage.vue
+++ components/BlurrableImage.vue
@@ -4,6 +4,20 @@ defineProps<{
   alt?: string
   placeholderDataUri?: string
 }>()
+
+const imgRef = ref<HTMLImageElement | null>(null)
+const imgLoaded = ref(false)
+const imgOpacity = computed(() => (imgLoaded.value ? 1 : 0))
+
+onMounted(() => {
+  if (!imgRef.value || imgRef.value.complete) {
+    return (imgLoaded.value = true)
+  }
+
+  imgRef.value.onload = () => {
+    imgLoaded.value = true
+  }
+})
 </script>
 
 <template>
@@ -11,7 +25,9 @@ defineProps<{
     :style="placeholderDataUri && { 'background-image': `url('${placeholderDataUri}')` }"
     :class="$style.wrapper"
   >
-    <img :src="src" :alt="alt" :class="$style.image" loading="lazy" />
+    <div :class="$style.imageWrapper">
+      <img ref="imgRef" :src="src" :alt="alt" :class="$style.image" loading="lazy" />
+    </div>
   </div>
 </template>
 
@@ -22,10 +38,18 @@ defineProps<{
   background-size: cover;
 }
 
+.imageWrapper {
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(12px);
+}
+
 .image {
+  display: block;
   width: 100%;
   height: 100%;
   object-fit: cover;
-  backdrop-filter: blur(8px);
+  opacity: v-bind(imgOpacity);
+  transition: opacity 0.2s ease-in-out;
 }
 </style>
