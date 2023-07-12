f5ed71bac51371729d5cc32107e89a5135b8cedb
dogyeong <shroad1802@naver.com>
2023-07-12 15:36:09 +0900

    feat: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,48 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/robots', 'nuxt-svgo'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      link: [
+        { rel: 'apple-touch-icon-precomposed', sizes: '57x57', href: 'favicon/apple-touch-icon-57x57.png' },
+        {
+          rel: 'apple-touch-icon-precomposed',
+          sizes: '114x114',
+          href: 'favicon/apple-touch-icon-114x114.png',
+        },
+        { rel: 'apple-touch-icon-precomposed', sizes: '72x72', href: 'favicon/apple-touch-icon-72x72.png' },
+        {
+          rel: 'apple-touch-icon-precomposed',
+          sizes: '144x144',
+          href: 'favicon/apple-touch-icon-144x144.png',
+        },
+        { rel: 'apple-touch-icon-precomposed', sizes: '60x60', href: 'favicon/apple-touch-icon-60x60.png' },
+        {
+          rel: 'apple-touch-icon-precomposed',
+          sizes: '120x120',
+          href: 'favicon/apple-touch-icon-120x120.png',
+        },
+        { rel: 'apple-touch-icon-precomposed', sizes: '76x76', href: 'favicon/apple-touch-icon-76x76.png' },
+        {
+          rel: 'apple-touch-icon-precomposed',
+          sizes: '152x152',
+          href: 'favicon/apple-touch-icon-152x152.png',
+        },
+        { rel: 'icon', type: 'image/png', href: 'favicon/favicon-196x196.png', sizes: '196x196' },
+        { rel: 'icon', type: 'image/png', href: 'favicon/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/png', href: 'favicon/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: 'favicon/favicon-16x16.png', sizes: '16x16' },
+        { rel: 'icon', type: 'image/png', href: 'favicon/favicon-128.png', sizes: '128x128' },
+      ],
+      meta: [
+        { name: 'application-name', content: 'dogyeong.me' },
+        { name: 'msapplication-TileColor', content: '#FFFFFF' },
+        { name: 'msapplication-TileImage', content: 'favicon/mstile-144x144.png' },
+        { name: 'msapplication-square70x70logo', content: 'favicon/mstile-70x70.png' },
+        { name: 'msapplication-square150x150logo', content: 'favicon/mstile-150x150.png' },
+        { name: 'msapplication-wide310x150logo', content: 'favicon/mstile-310x150.png' },
+        { name: 'msapplication-square310x310logo', content: 'favicon/mstile-310x310.png' },
+      ],
+    },
   },
   nitro: {
     prerender: {
