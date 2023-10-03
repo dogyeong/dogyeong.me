105a12fbef3c237ab419c1a728c151da368c7d1b
dogyeong <shroad1802@naver.com>
2023-10-03 15:24:59 +0900

    feat: change inline code style

--- components/content/ProseCodeInline.vue
+++ components/content/ProseCodeInline.vue
@@ -7,5 +7,9 @@
   font-family: 'Source Code Pro', monospace;
   color: $grey-3;
   font-weight: 600;
+  background-color: $grey-9;
+  font-size: 0.875em;
+  padding: 2px 6px;
+  border-radius: 4px;
 }
 </style>
